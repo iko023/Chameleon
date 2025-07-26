e970fd575e6751b0dee5e102b24a538e8d868373
Ilya Korneev <iko023@uib.no>
2025-07-26 23:23:39 +1000

    added storage fore words and players, and added funcion to register player

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, jsonify
+import random, uuid, time
 
 app = Flask(__name__)
+app.secret_key = 'your secret key'
+
+viewers = {} # Store game participants
+# store words
+word_list = [
+    'apple',
+    'banana',
+    'cherry',
+    'dragon',
+    'elephant'
+]
+# store game status, word, and chameleon
+game = {
+    'started' : False,
+    'word' : None,
+    'chameleon' : None
+}
+
+# register viewer or participant on erery status poll
+def register_viewer():
+  sid = session.get('sid')
+  if not sid:
+    sid = str(uuid.uuid4())
+    session['sid'] = sid
+  viewers[sid] = {
+    'ip': request.remote_addr,
+    'last_seen': time.time()
+  }
 
 
-# @app.route('/')
-# def hello():
-#     return "<p>Hello James! I love you!</p>"
+
 
 @app.route('/')
 def index():
