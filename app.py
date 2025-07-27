3dc3329d180c91a09681bb41deeb0d818ec1f283
Ilya Korneev <iko023@uib.no>
2025-07-28 08:33:23 +1000

    Moved word list to seperate file. Cleaned up app.py and added empty readme

--- app.py
+++ app.py
@@ -1,74 +1,16 @@
 from flask import Flask, render_template, request, session, jsonify
 import random, uuid, time
+from words import word_list
 
 app = Flask(__name__)
-app.secret_key = 'your secret key'
+app.secret_key = 'ogisuhvm32989wifkasnmqi0ewnwfe'
 
-viewers = {} # Store game participants
-# store words
-word_list = [
-    "Jungle",
-    "Beach",
-    "Space",
-    "Boat",
-    "Office",
-    "North Pole",
-    "Hospital",
-    "Cinema",
-    "Forest",
-    "Restaurant",
-    "Mountains",
-    "Zoo",
-    "Farm",
-    "Mall",
-    "School",
-    "Park",
-    "Library",
-    "Airport",
-    "Stadium",
-    "Castle",
-    "Aquarium",
-    "Museum",
-    "Factory",
-    "Concert Hall",
-    "Subway Station",
-    "Island",
-    "Canyon",
-    "Desert",
-    "Pub",
-    "Café",
-    "Bank",
-    "Grocery Store",
-    "Hotel",
-    "Warehouse",
-    "Garage",
-    "Laboratory",
-    "Theater",
-    "Playground",
-    "Gym",
-    "Salon",
-    "Spa",
-    "Bus",
-    "Airplane",
-    "Jurassic Park"
-]
+#################################################
+# VARIABLES
+#################################################
 
+players = {}
 bag = []
-
-# Bag of words to reduce redraw
-def init_bag():
-    global bag
-    bag = list(set(word_list.copy()))
-    random.shuffle(bag)
-
-# Draw word and remove from bag
-def draw_word():
-    global bag
-    if not bag:
-        init_bag()
-    return bag.pop()
-
-# store game status, word, and chameleon
 game = {
     'started' : False,
     'word' : None,
@@ -76,42 +18,64 @@ game = {
     'words' : bag
 }
 
-# register viewer or participant on erery status poll
-def register_viewer():
+#################################################
+# FUNCTIONS
+#################################################
+
+
+def init_bag():
+    """ Initiate bag of words. """
+    global bag
+    bag = list(set(word_list.copy()))
+    random.shuffle(bag)
+
+
+def draw_word():
+    """ Draw and remove word from bag. """
+    global bag
+    if not bag:
+        init_bag()
+    return bag.pop()
+
+
+def register_player():
+  """ Registers new ip adresses as players. """
   sid = session.get('sid')
   if not sid:
     sid = str(uuid.uuid4())
     session['sid'] = sid
-  viewers[sid] = {
+  players[sid] = {
     'ip': request.remote_addr,
     'last_seen': time.time()
   }
 
-# Registers player, removes inactive players, returns current playercount
+
 @app.route('/status')
 def status():
-    register_viewer()
-    cutoff = time.time() - 30
-    for sid, info in list(viewers.items()):
+    """ Registers player and removes inactive players after 5 sec. Returns player count. """
+    register_player()
+    cutoff = time.time() - 5                  
+    for sid, info in list(players.items()):
         if info['last_seen'] < cutoff:
-            viewers.pop(sid)
-    # Only return count now
-    return jsonify(count=len(viewers))
+            players.pop(sid)                   
+    return jsonify(count=len(players))
 
-# Start game
+
 @app.route('/start', methods=['POST'])
 def start_game():
-  if not game['started'] and viewers:
-    sids = list(viewers.keys())
+  """ Assigns chameleon, draws word and starts game. """
+  if not game['started'] and players:
+    sids = list(players.keys())
     game['chameleon'] = random.choice(sids)
     game['word'] = draw_word()
     game['started'] = True
   return jsonify({'status': 'ok'})
 
-# Get game statet
+
 @app.route('/game_state')
 def game_state():
-  register_viewer()
+  """ Retreaves game state and registes player. """
+  register_player()
   sid = session.get('sid')
   payload = {
     'started': game['started'],
@@ -120,17 +84,22 @@ def game_state():
   }
   return jsonify(payload)
 
-# Quit/restart
+
 @app.route('/reset', methods=['POST'])
 def reset_game():
+  """ Restarts game and clears game state. """
   game.update({'started': False, 'word': None, 'chameleon': None})
   return jsonify({'status': 'reset'})
 
 
 @app.route('/')
 def index():
+    """ Renders html. """
     return render_template('index.html')
 
+
+
+
 if __name__ == '__main__':
-  init_bag()
-  app.run(debug=True)
+  init_bag()                # Initiates bag of words
+  app.run(debug=True)       # Runs app
